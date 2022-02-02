d598f4b485b6937d6f1e19e30e5d4b98c67cd55b
Fill237 <andreeva.mariya94@bk.ru>
2022-02-02 10:55:56 +0300

    Variables

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        double num1 = 827.56; //переменная с плавающей точкой с наибольшим диапозоном чисел
+        byte num2 = 15; //переменная целочисленного типа с наименьшим диапозоном чисел
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+}
